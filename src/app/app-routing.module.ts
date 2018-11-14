fa3cd6774612e315ab4f74729200c484ecba526b
MeenaS <smeena.ece13@gmail.com>
2018-11-14 16:04:47 +0530

    to the github

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 import { GoatComponent } from './goat/goat.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { PaymentComponent } from './payment/payment.component';
+
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'goat', component: GoatComponent }
+  { path: 'goat', component: GoatComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'payment', component: PaymentComponent }
  
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,10 @@ import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { GoatComponent } from './goat/goat.component';
+import { ChickenComponent } from './chicken/chicken.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { PaymentComponent } from './payment/payment.component';
 
 
 
@@ -22,7 +26,11 @@ const rootRouting: ModuleWithProviders = RouterModule.forRoot([], { useHash: tru
     FooterComponent,
     HeaderComponent,
 	HomeComponent,
-	GoatComponent
+	GoatComponent,
+	ChickenComponent,
+	LoginComponent,
+	SignupComponent,
+	PaymentComponent
 	
     
   ],
